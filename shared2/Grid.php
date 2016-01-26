8eaa16fc37b9c967b4db7079c7840fb3540918a3
mbassan <moises@organic.com.pa>
2016-01-26 20:33:51 +0000

    Grid rendering issue.

--- shared2/Grid.php
+++ shared2/Grid.php
@@ -1279,11 +1279,10 @@ class Grid {
 		echo $HTML;
 		
 		if ($this->rows_per_page > 30) echo $pagination;
-		
 		if ($CFG->backstage_mode && (User::permission(0,0,$this->link_url,false,$this->is_tab) > 1) && $this->inset_id === false) {
 			echo "</form>";
 			
-			if (!($this->inset_id > 0 || $CFG->is_form_inset))
+			if ($this->inset_id === false && !$CFG->is_form_inset)
 				echo '</div></div>';
 		}
 	}
